9478a2915a54495a27e7f48828ed2ca3e274b3fb
Markus Felten <Markus.Felten@gmx.de>
2020-04-02 15:46:56 +0200

    docs: add sample

--- src/one-time-execution-method.mjs
+++ src/one-time-execution-method.mjs
@@ -1,16 +1,26 @@
 
 /**
+ * For async functions the resulting Promise of the 1st. invocation
+ * will be preserved and always delivered in the future.
+ * ```js
+ * class MyClass { ... }
+ * defineOneTimeExecutionMethod(MyClass.prototype, "initialize", async function() {
+ *  // code here will be executed only once
+ * });
  * 
+ * const object = new MyClass();
+ * object.initialize(); // body will be executed only once
+ * ```
  * @param {Object} object prototype to bind method against 
  * @param {string} name of the method  
- * @param {Function} func to be executed (once) 
+ * @param {Function} func to be executed (once) must deliver a Promise 
  */
 export function defineOneTimeExecutionMethod(object, name, func) {
   /**
    * undefined -> call func and store Promise
    * Promise   -> func currently running or fullfilled -> deliver this Promise
    */
-  const transitionState = Symbol(`OneTimeExecutionState:${name}`);
+  const transitionState = Symbol(`OneTimeExecutionState<${name}>`);
 
   Object.defineProperty(object, name, {
     value: async function(...args) {
--- tests/one-time-execution-method-test.mjs
+++ tests/one-time-execution-method-test.mjs
@@ -9,13 +9,14 @@ class MyClass {
   }
 }
 
-defineOneTimeExecutionMethod(MyClass.prototype, "initialize", async function() {
+defineOneTimeExecutionMethod(MyClass.prototype, "initialize", function() {
   this.executions++;
   return new Promise(resolve => setTimeout(resolve, 200));
 });
 
 test("defineOneTimeExecutionMethod once", async t => {
   const object = new MyClass();
+  t.is(object.executions, 0);
   await object.initialize()
   t.is(object.executions, 1);
 });
@@ -23,6 +24,7 @@ test("defineOneTimeExecutionMethod once", async t => {
 test("defineOneTimeExecutionMethod parallel", async t => {
   const object = new MyClass();
 
+  t.is(object.executions, 0);
   Promise.all([object.initialize(), object.initialize(), object.initialize()]);
 
   await object.initialize();
